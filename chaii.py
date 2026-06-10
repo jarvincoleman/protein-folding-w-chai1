c5b85db91221e6e03c83d16081d77a24fe6780bb
coleman-jarvin <colemanjarvin13@gmail.com>
2026-06-10 18:46:33 -0500

    fixd relative paths

--- chaii.py
+++ chaii.py
@@ -8,14 +8,14 @@ from chai_lab.chai1 import run_inference
 fasta_path = Path("/kaggle/working/protein-folding-w-chai1/input.fasta")
 
 
-output_dir = Path("/kaggle/working/protein-folding-w-chai1/outputs")
+output_dir = Path("/kaggle/working/outputs")
 if output_dir.exists():
     shutil.rmtree(output_dir)
 output_dir.mkdir(exist_ok=True) 
 
 candidates = run_inference(
     fasta_file = fasta_path, 
-    output_dir = Path("outputs"),
+    output_dir = output_dir,
     num_trunk_recycles=3,
     num_diffn_timesteps=200, 
     num_diffn_samples=1, 
