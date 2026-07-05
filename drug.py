3cf95eb09d7f5b4c465dc5a9e4f0d11a8759e4b6
coleman-jarvin <colemanjarvin13@gmail.com>
2026-07-04 22:45:24 -0500

    we here

--- drug.py
+++ drug.py
@@ -1,17 +1,24 @@
 import py3Dmol 
 from chai_lab.chai1 import run_inference 
-
+from chaii.py import fasta_path
 
 
 
 
 class DrugInference:
     def __init__(self, abrv_name, id, rna_type):
-        self.name = name 
-        self.id = d
+        self.abrv_name = abrv_name
+        self.id = id 
+        self.rna_type = rna_type
 
 
-    def classify(self, pair):
+        id = [] 
+
+        
+
+
+    def classify(self, id):
+
         pass
 
     def match(self):
